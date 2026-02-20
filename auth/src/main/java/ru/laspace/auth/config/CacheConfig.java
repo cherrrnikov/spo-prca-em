d9d8eecef5de620e4397d77f502553d7cf144f2c
adm1n <adm1n@adm1n.com>
2026-02-20 10:10:16 +0300

    refactor: cache, security и swagger конфиги

--- auth/src/main/java/ru/laspace/auth/config/CacheConfig.java
+++ auth/src/main/java/ru/laspace/auth/config/CacheConfig.java
@@ -2,7 +2,6 @@ package ru.laspace.auth.config;
 
 import java.time.Duration;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,43 +12,23 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
-        @SuppressWarnings("null")
         @Bean
-        public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-                ObjectMapper objectMapper = new ObjectMapper();
-
-                objectMapper.activateDefaultTyping(
-                                objectMapper.getPolymorphicTypeValidator(),
-                                ObjectMapper.DefaultTyping.NON_FINAL,
-                                JsonTypeInfo.As.PROPERTY);
-
-                objectMapper.registerModule(new JavaTimeModule());
-                objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-                objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-
-                GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-
+        public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
                 RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.ofMinutes(30))
+                                .entryTtl(Duration.ofMinutes(10))
                                 .disableCachingNullValues()
-                                .serializeKeysWith(RedisSerializationContext.SerializationPair
-                                                .fromSerializer(new StringRedisSerializer()))
+                                .serializeKeysWith(
+                                                RedisSerializationContext.SerializationPair
+                                                                .fromSerializer(new StringRedisSerializer()))
                                 .serializeValuesWith(
-                                                RedisSerializationContext.SerializationPair.fromSerializer(serializer));
-
+                                                RedisSerializationContext.SerializationPair.fromSerializer(
+                                                                new GenericJackson2JsonRedisSerializer()));
                 return RedisCacheManager.builder(redisConnectionFactory)
                                 .cacheDefaults(config)
-                                .transactionAware()
                                 .build();
         }
 }
