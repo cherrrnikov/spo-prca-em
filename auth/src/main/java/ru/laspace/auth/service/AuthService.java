b47b8308c82f52acda94aa87c7fa49697f3ff292
adm1n <adm1n@adm1n.com>
2026-02-20 14:01:29 +0300

    refactor: переделан AuthService

--- auth/src/main/java/ru/laspace/auth/service/AuthService.java
+++ auth/src/main/java/ru/laspace/auth/service/AuthService.java
@@ -1,13 +1,14 @@
 package ru.laspace.auth.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import ru.laspace.auth.dto.request.LoginRequest;
 import ru.laspace.auth.dto.request.RefreshTokenRequest;
 import ru.laspace.auth.dto.response.JwtResponse;
 
 public interface AuthService {
-    JwtResponse login(LoginRequest loginRequest);
+    JwtResponse login(LoginRequest request, HttpServletRequest httpRequest);
 
-    JwtResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
+    JwtResponse refresh(RefreshTokenRequest request);
 
     void logout(String refreshToken);
 }
