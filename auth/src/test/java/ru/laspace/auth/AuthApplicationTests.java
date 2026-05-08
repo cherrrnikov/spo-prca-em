68842c3898ab2c4a84966be6d2684b646bad264c
adm1n <adm1n@adm1n.com>
2026-05-08 14:32:12 +0300

    fix: add redis testcontainer

--- auth/src/test/java/ru/laspace/auth/AuthApplicationTests.java
+++ auth/src/test/java/ru/laspace/auth/AuthApplicationTests.java
@@ -2,8 +2,10 @@ package ru.laspace.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest(classes = AuthApplication.class)
+@Import(TestcontainersConfiguration.class)
 class AuthApplicationTests {
 
 	@Test
--- auth/src/test/java/ru/laspace/auth/TestcontainersConfiguration.java
+++ auth/src/test/java/ru/laspace/auth/TestcontainersConfiguration.java
@@ -3,6 +3,7 @@ package ru.laspace.auth;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -15,4 +16,10 @@ public class TestcontainersConfiguration {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
     }
 
+    @Bean
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+                .withExposedPorts(6379);
+    }
 }
